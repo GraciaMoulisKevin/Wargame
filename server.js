9152d95eb518770de7f86bc450258d8886d4ef45
Reiter Maxime <maxime.reiter@etu.umontpellier.fr>
2020-01-28 10:34:56 +0100

    Alpha Lobby

---
+++ server.js
@@ -0,0 +1,36 @@
+const app = require('express')();
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const port = 3000
+const bodyParser = require('body-parser')
+const fs = require('fs') // filesystem
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/room/*', (req,res) => {
+    res.sendFile(__dirname + '/room/' + req.originalUrl.slice(6) + '.html', (err) => {
+        if(err){
+            res.redirect('/');
+        }
+    });
+})
+
+app.post('/', (req,res) => {
+    res.redirect('/room/' + req.body.roomname);
+})
+
+io.on('connection', function(socket){
+    const files = fs.readdirSync(__dirname + '/room/');
+    socket.emit('files',files);
+})
+
+server.listen(port, () => {
+    console.log(`Running on ${port}!`)
+})
