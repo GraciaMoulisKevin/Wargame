510b7fb04224a8df4b220379c2854c5609e2768b
C.Thomas <thomas.canta@etu.umontpellier.fr>
2020-02-27 15:39:40 +0100

    create simple map_editor, have to be upgrade with a python script that write properly everything in a .json file

---
+++ map_editor.js
@@ -0,0 +1,13 @@
+function startEdition(type){
+    d3.selectAll(".hexagon")
+        .on("click", function(){
+            d3.select(this)
+                .attr("data-type", type);
+        })
+}
+function stopEdition(type){
+    d3.selectAll(".hexagon")
+        .on("click", function(){
+            onclickHexagonEvent(this);
+    })
+}
